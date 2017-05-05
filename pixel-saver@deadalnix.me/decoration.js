ed0c662de1c22d76f6c54aca27fe141ccb1aee96
Jan Niklas Hasse <jhasse@bixense.com>
2017-05-05 20:47:55 +0200

    Remove workaround for GNOME Shell bug
    See https://bugzilla.gnome.org/show_bug.cgi?id=781862

--- pixel-saver@deadalnix.me/decoration.js
+++ pixel-saver@deadalnix.me/decoration.js
@@ -35,7 +35,7 @@ function guessWindowXID(win) {
 	if (win._pixelSaverWindowID) {
 		return win._pixelSaverWindowID;
 	}
-	
+
 	/**
 	 * If window title has non-utf8 characters, get_description() complains
 	 * "Failed to convert UTF-8 string to JS string: Invalid byte sequence in conversion input",
@@ -47,7 +47,7 @@ function guessWindowXID(win) {
 			return win._pixelSaverWindowID = m[0];
 		}
 	} catch (err) { }
-	
+
 	// use xwininfo, take first child.
 	let act = win.get_compositor_private();
 	let xwindow = act && act['x-window'];
@@ -55,7 +55,7 @@ function guessWindowXID(win) {
 		let xwininfo = GLib.spawn_command_line_sync('xwininfo -children -id 0x%x'.format(xwindow));
 		if (xwininfo[0]) {
 			let str = xwininfo[1].toString();
-			
+
 			/**
 			 * The X ID of the window is the one preceding the target window's title.
 			 * This is to handle cases where the window has no frame and so
@@ -66,7 +66,7 @@ function guessWindowXID(win) {
 			if (m && m[1]) {
 				return win._pixelSaverWindowID = m[1];
 			}
-			
+
 			// Otherwise, just grab the child and hope for the best
 			m = str.split(/child(?:ren)?:/)[1].match(/0x[0-9a-f]+/);
 			if (m && m[0]) {
@@ -74,7 +74,7 @@ function guessWindowXID(win) {
 			}
 		}
 	}
-	
+
 	// Try enumerating all available windows and match the title. Note that this
 	// may be necessary if the title contains special characters and `x-window`
 	// is not available.
@@ -82,16 +82,16 @@ function guessWindowXID(win) {
 	LOG('xprop -root _NET_CLIENT_LIST')
 	if (result[0]) {
 		let str = result[1].toString();
-		
+
 		// Get the list of window IDs.
 		let windowList = str.match(/0x[0-9a-f]+/g);
-		
+
 		// For each window ID, check if the title matches the desired title.
 		for (var i = 0; i < windowList.length; ++i) {
 			let cmd = 'xprop -id "' + windowList[i] + '" _NET_WM_NAME _PIXEL_SAVER_ORIGINAL_STATE';
 			let result = GLib.spawn_command_line_sync(cmd);
 			LOG(cmd);
-			
+
 			if (result[0]) {
 				let output = result[1].toString();
 				let isManaged = output.indexOf("_PIXEL_SAVER_ORIGINAL_STATE(CARDINAL)") > -1;
@@ -125,28 +125,28 @@ const WindowState = {
 /**
  * Get the value of _GTK_HIDE_TITLEBAR_WHEN_MAXIMIZED before
  * pixel saver did its magic.
- * 
+ *
  * @param {Meta.Window} win - the window to check the property
  */
 function getOriginalState(win) {
 	if (win._pixelSaverOriginalState !== undefined) {
 		return win._pixelSaverOriginalState;
 	}
-	
+
 	if (!win.decorated) {
 		return win._pixelSaverOriginalState = WindowState.UNDECORATED;
 	}
-	
+
 	let id = guessWindowXID(win);
 	let cmd = 'xprop -id ' + id;
 	LOG(cmd);
-	
+
 	let xprops = GLib.spawn_command_line_sync(cmd);
 	if (!xprops[0]) {
 		WARN("xprop failed for " + win.title + " with id " + id);
 		return win._pixelSaverOriginalState = State.UNKNOWN;
 	}
-	
+
 	let str = xprops[1].toString();
 	let m = str.match(/^_PIXEL_SAVER_ORIGINAL_STATE\(CARDINAL\) = ([0-9]+)$/m);
 	if (m) {
@@ -154,7 +154,7 @@ function getOriginalState(win) {
 			? WindowState.HIDE_TITLEBAR
 			: WindowState.DEFAULT;
 	}
-	
+
 	m = str.match(/^_GTK_HIDE_TITLEBAR_WHEN_MAXIMIZED(\(CARDINAL\))? = ([0-9]+)$/m);
 	if (m) {
 		let state = !!m[1];
@@ -168,9 +168,9 @@ function getOriginalState(win) {
 			? WindowState.HIDE_TITLEBAR
 			: WindowState.DEFAULT;
 	}
-	
+
 	WARN("Can't find original state for " + win.title + " with id " + id);
-	
+
 	// GTK uses the _GTK_HIDE_TITLEBAR_WHEN_MAXIMIZED atom to indicate that the
 	// title bar should be hidden when maximized. If we can't find this atom, the
 	// window uses the default behavior
@@ -186,7 +186,7 @@ function getOriginalState(win) {
  *
  * **Caveat**: doesn't work with Ubuntu's Ambiance and Radiance window themes -
  * my guess is they don't respect or implement this property.
- * 
+ *
  * I don't know how to read the inital value, so I'm not sure how to resore it.
  *
  * @param {Meta.Window} win - window to set the HIDE_TITLEBAR_WHEN_MAXIMIZED property of.
@@ -194,10 +194,10 @@ function getOriginalState(win) {
  */
 function setHideTitlebar(win, hide) {
 	LOG('setHideTitlebar: ' + win.get_title() + ': ' + hide);
-	
+
 	// Make sure we save the state before altering it.
 	getOriginalState(win);
-	
+
 	/**
 	 * Undecorate with xprop. Use _GTK_HIDE_TITLEBAR_WHEN_MAXIMIZED.
 	 * See (eg) mutter/src/window-props.c
@@ -207,7 +207,7 @@ function setHideTitlebar(win, hide) {
 	           '-set', '_GTK_HIDE_TITLEBAR_WHEN_MAXIMIZED',
 	           (hide ? '0x1' : '0x0')];
 	LOG(cmd.join(' '));
-	
+
 	// Run xprop
 	[success, pid] = GLib.spawn_async(
 		null,
@@ -215,22 +215,6 @@ function setHideTitlebar(win, hide) {
 		null,
 		GLib.SpawnFlags.SEARCH_PATH | GLib.SpawnFlags.DO_NOT_REAP_CHILD,
 		null);
-	
-	// After xprop completes, unmaximize and remaximize any window
-	// that is already maximized. It seems that setting the xprop on
-	// a window that is already maximized doesn't actually take
-	// effect immediately but it needs a focuse change or other
-	// action to force a relayout. Doing unmaximize and maximize
-	// here seems to be an uninvasive way to handle this. This needs
-	// to happen _after_ xprop completes.
-	GLib.child_watch_add(GLib.PRIORITY_DEFAULT, pid, function () {
-		const MAXIMIZED = Meta.MaximizeFlags.BOTH;
-		let flags = win.get_maximized();
-		if (flags == MAXIMIZED) {
-			win.unmaximize(MAXIMIZED);
-			win.maximize(MAXIMIZED);
-		}
-	});
 }
 
 /**** Callbacks ****/
@@ -250,14 +234,14 @@ function onWindowAdded(ws, win, retry) {
 	if (win.window_type === Meta.WindowType.DESKTOP) {
 		return false;
 	}
-	
+
 	// If the window is simply switching workspaces, it will trigger a
 	// window-added signal. We don't want to reprocess it then because we already
 	// have.
 	if (win._pixelSaverOriginalState !== undefined) {
 		return false;
 	}
-	
+
 	/**
 	 * Newly-created windows are added to the workspace before
 	 * the compositor knows about them: get_compositor_private() is null.
@@ -269,14 +253,14 @@ function onWindowAdded(ws, win, retry) {
 		if (retry > 3) {
 			return false;
 		}
-		
+
 		Mainloop.idle_add(function () {
 			onWindowAdded(ws, win, retry + 1);
 			return false;
 		});
 		return false;
 	}
-	
+
 	retry = 3;
 	Mainloop.idle_add(function () {
 		let id = guessWindowXID(win);
@@ -284,16 +268,16 @@ function onWindowAdded(ws, win, retry) {
 			if (--retry) {
 				return true;
 			}
-			
+
 			WARN("Finding XID for window %s failed".format(win.title));
 			return false;
 		}
-		
+
 		LOG('onWindowAdded: ' + win.get_title());
 		setHideTitlebar(win, true);
 		return false;
 	});
-	
+
 	return false;
 }
 
@@ -309,7 +293,7 @@ let workspaces = [];
  */
 function onChangeNWorkspaces() {
 	cleanWorkspaces();
-	
+
 	let i = global.screen.n_workspaces;
 	while (i--) {
 		let ws = global.screen.get_workspace_by_index(i);
@@ -320,7 +304,7 @@ function onChangeNWorkspaces() {
 			Mainloop.idle_add(function () { return onWindowAdded(ws, win); });
 		});
 	}
-	
+
 	return false;
 }
 
@@ -333,7 +317,7 @@ function cleanWorkspaces() {
 		ws.disconnect(ws._pixelSaverWindowAddedId);
 		delete ws._pixelSaverWindowAddedId;
 	});
-	
+
 	workspaces = [];
 }
 
@@ -353,7 +337,7 @@ let changeWorkspaceID = 0;
 function enable() {
 	// Connect events
 	changeWorkspaceID = global.screen.connect('notify::n-workspaces', onChangeNWorkspaces);
-	
+
 	/**
 	 * Go through already-maximised windows & undecorate.
 	 * This needs a delay as the window list is not yet loaded
@@ -368,7 +352,7 @@ function enable() {
 		forEachWindow(function(win) {
 			onWindowAdded(null, win);
 		});
-		
+
 		onChangeNWorkspaces();
 		return false;
 	});
@@ -379,16 +363,16 @@ function disable() {
 		global.screen.disconnect(changeWorkspaceID);
 		changeWorkspaceID = 0;
 	}
-	
+
 	cleanWorkspaces();
-	
+
 	forEachWindow(function(win) {
 		let state = getOriginalState(win);
 		LOG('stopUndecorating: ' + win.title + ' original=' + state);
 		if (state == WindowState.DEFAULT) {
 			setHideTitlebar(win, false);
 		}
-		
+
 		delete win._pixelSaverOriginalState;
 	});
 }
